87b13b5d2a2b67865d94ed0907c0adb8bae75f37
Shivam0Anand <shivam_anand@outlook.com>
2019-09-05 00:22:51 +0530

    modified:app.js

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const url =
   "https://api.darksky.net/forecast/b4c116672bb90062c3cf3ed6171b5d07/28.474388,77.503990";
 
 request({ url: url, json: true }, (error, responce) => {
-  console.log(responce.body.currently);
+  console.log(
+    "It is currently " +
+      responce.body.currently.temperature +
+      " degrees out. There is " +
+      responce.body.currently.precipProbability +
+      "% chance of rain."
+  );
 });
