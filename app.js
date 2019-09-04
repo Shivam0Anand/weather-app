8eed08ea6d42c2e0c797aab22ae5cb545a7c16c3
Shivam0Anand <shivam_anand@outlook.com>
2019-09-04 15:52:12 +0530

    modified:app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-console.log("initialize");
-
 const request = require("request");
-request("https://erp.aktu.ac.in", (error, responce, body) => {
-  console.log("error", error);
-  console.log("statusCode", responce && responce.statusCode);
-  console.log("body", body);
-});
 
-console.log("Stopping");
+const url =
+  "https://api.darksky.net/forecast/b4c116672bb90062c3cf3ed6171b5d07/28.474388,77.503990";
+
+request({ url: url, json: true }, (error, responce) => {
+  console.log(responce.body.currently);
+});
