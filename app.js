a02eeec8b056977e8ab0e776130e208382b93439
Shivam0Anand <shivam_anand@outlook.com>
2019-09-05 01:06:59 +0530

    modified:app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/b4c116672bb90062c3cf3ed6171b5d07/28.474388,77.503990";
+  "https://api.darksky.net/forecast/b4c116672bb90062c3cf3ed6171b5d07/28.474388,77.503990?units=si";
 
 request({ url: url, json: true }, (error, responce) => {
   console.log(
-    "It is currently " +
+    responce.body.daily.data[0].summary +
+      ". It is currently " +
       responce.body.currently.temperature +
       " degrees out. There is " +
       responce.body.currently.precipProbability +
