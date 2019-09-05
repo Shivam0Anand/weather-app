a3657aa04f7355e39bebde042896bf1e4c42b120
Shivam0Anand <shivam_anand@outlook.com>
2019-09-05 22:30:42 +0530

    modified:app.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -8,19 +8,19 @@ const forecast = (latitude, longitude, callback) => {
     longitude +
     "?units=si";
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect!", undefined);
-    } else if (response.body.error) {
+      callback("Internet Connect kr BC!", undefined);
+    } else if (body.error) {
       callback("Unable to find location!", undefined);
     } else {
       callback(
         undefined,
-        response.body.daily.data[0].summary +
+        body.daily.data[0].summary +
           ". It is currently " +
-          response.body.currently.temperature +
+          body.currently.temperature +
           " degrees out. There is " +
-          response.body.currently.precipProbability +
+          body.currently.precipProbability +
           "% chance of rain."
       );
     }
