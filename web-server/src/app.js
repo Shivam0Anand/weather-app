0d780ad69e516e34668265e40fb150a6f64db580
Shivam0Anand <shivam_anand@outlook.com>
2019-09-06 16:47:26 +0530

    modified

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,39 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+// Define Paths for express
 const pathPublicDirectory = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
+// setup static directory to serve
 app.use(express.static(pathPublicDirectory));
 
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
     name: "Shivam Anand"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Shivam Anand"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "Message"
   });
 });
 
